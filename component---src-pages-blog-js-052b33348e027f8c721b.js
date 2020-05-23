f5af3ed9eb70ca88b7de3e188b9a0487b2cc6c1d
John Haldeman <john.haldeman@infoinsightsllc.com>
2020-05-23 18:20:20 -0400

    Improved <head> handling

---
+++ component---src-pages-blog-js-052b33348e027f8c721b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{vx99:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return l}));var a=t("q1tI"),o=t.n(a),r=t("qV08");function l(){return o.a.createElement(o.a.Fragment,null,o.a.createElement(r.a,{title:"Blog"}),o.a.createElement("h3",null,"Coming Soon!"))}}}]);
+//# sourceMappingURL=component---src-pages-blog-js-052b33348e027f8c721b.js.map
